22502376cfe69498631807fdb267fbf8565b9223
Jacky Efendi <jacky.efendi1@gmail.com>
2020-02-07 21:15:52 +0700

    first

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -ex
 
 echo "Building lighthouse-puppeteer-custom docker image..."
+chmod +x ./docker
+chmod +x ./docker/*
 GIT=`git rev-parse HEAD`
 GIT_SHORT=`git rev-parse --short HEAD`
 
@@ -11,21 +13,21 @@ docker build \
     --tag lighthouse-puppeteer-custom:latest \
     .
 
-# docker build \
-#     --file ./DockerFile \
-#     --label commit=$GIT \
-#     --label commit_short=$GIT_SHORT \
-#     --tag jackyef/lighthouse-puppeteer-custom:latest \
-#     .
+docker build \
+    --file ./DockerFile \
+    --label commit=$GIT \
+    --label commit_short=$GIT_SHORT \
+    --tag jackyef/lighthouse-puppeteer-custom:latest \
+    .
 
-# docker build \
-#     --file ./DockerFile \
-#     --label commit=$GIT \
-#     --label commit_short=$GIT_SHORT \
-#     --tag jackyef/lighthouse-puppeteer-custom:$GIT \
-#     .
+docker build \
+    --file ./DockerFile \
+    --label commit=$GIT \
+    --label commit_short=$GIT_SHORT \
+    --tag jackyef/lighthouse-puppeteer-custom:$GIT \
+    .
 
-# docker push jackyef/lighthouse-puppeteer-custom:latest
-# docker push jackyef/lighthouse-puppeteer-custom:$GIT
+docker push jackyef/lighthouse-puppeteer-custom:latest
+docker push jackyef/lighthouse-puppeteer-custom:$GIT
 
 echo "✅ All done!"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,6 @@ CHROME_FLAGS=${CHROME_FLAGS:-"--headless --disable-gpu --no-sandbox"}
 # first arg is `-f` or `--some-option`
 if [ "${1#http}" != "$1" ]; then
     node index.js --enable-error-reporting --chrome-flags="${CHROME_FLAGS}" "$@"
+else 
+    node /code/index.js "$@"
 fi
-
-node index.js "$@"
